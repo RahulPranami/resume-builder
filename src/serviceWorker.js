d1ad65e146e44cdf0363f7647b16a0b526b93ccf
RahulPranami <rahulpranami101@gmail.com>
2022-06-24 21:30:49 +0530

    adding service worker to load website faster using cache

---
+++ src/serviceWorker.js
@@ -0,0 +1,40 @@
+const isLocalhost = Boolean(
+    window.location.hostname == 'localhost' ||
+    window.location.hostname == '[::1]' ||
+    window.location.hostname.match(/^127(?:\:25[0-5][2][0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
+export function register(config) {
+    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator)
+    {
+        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+        if (publicUrl.origin !== window.location.origin) {
+            return;
+        }
+
+        window.addEventListener('load', () => {
+            const swUrl = '${process.env.PUBLIC_URL}/service-worker.js';
+            if (isLocalhost) {
+                checkValidServiceWorker(swUrl, config);
+
+                navigator.serviceWorker.ready.then(() => {
+                    console.log(
+                        'This web page is being served cache-first by a service worker.'
+                    );
+                });
+            }
+            else {
+                registerValidSW(swUrl, config);
+            }
+        });
+    }
+}
+
+function registerValidSW(swUrl, config) {
+    navigator.serviceWorker.register(swUrl).then(registration => {
+        const installingWorker = registration.installing;
+        if (installingWoker == null) {
+            ReadableStreamDefaultControlle
+        }
+    })
+}
